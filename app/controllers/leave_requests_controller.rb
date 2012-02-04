5cf3a7ba90bc333c433b61285b6198d16eb2754a
Michal Pasierbski <michal.pasierbski@sooftly.com>
2012-02-04 19:26:59 +0100

    rfctr

--- app/controllers/leave_requests_controller.rb
+++ app/controllers/leave_requests_controller.rb
@@ -6,7 +6,7 @@ class LeaveRequestsController < ApplicationController
 
   def show
   	@since, @to, @total = params[:since], params[:to], params[:total]
-  	@year = Time.new( ( @since || @to || Time.now.year ) ).year
+  	@year = Time.new( @since || @to || Time.now.year ).year
   end
 
 end
